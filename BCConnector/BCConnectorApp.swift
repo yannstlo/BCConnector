3462200c13b07c2f6c0ba86ba149e4bb67733778
Yann Saint-Laurent <yann@yann.ca>
2024-08-06 08:47:08 -0700

    Initial Commit

---
+++ BCConnector/BCConnectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BCConnectorApp.swift
+//  BCConnector
+//
+//  Created by Yann Saint-Laurent on 2024-08-06.
+//
+
+import SwiftUI
+
+@main
+struct BCConnectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BCConnector/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BCConnector
+//
+//  Created by Yann Saint-Laurent on 2024-08-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

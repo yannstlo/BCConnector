2bc45fe248ff2a5e40251eeae127ae5cb85a3028
Yann Saint-Laurent <yann@yann.ca>
2024-08-06 09:57:02 -0700

    feat: Add URL handling for authentication

--- BCConnector/BCConnectorApp.swift
+++ BCConnector/BCConnectorApp.swift
@@ -1,17 +1,25 @@
-//
-//  BCConnectorApp.swift
-//  BCConnector
-//
-//  Created by Yann Saint-Laurent on 2024-08-06.
-//
-
 import SwiftUI
 
 @main
 struct BCConnectorApp: App {
+    @StateObject private var authManager = AuthenticationManager.shared
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onOpenURL { url in
+                    Task {
+                        await handleURL(url)
+                    }
+                }
+        }
+    }
+    
+    private func handleURL(_ url: URL) async {
+        do {
+            try await authManager.handleRedirect(url: url)
+        } catch {
+            print("Error handling redirect: \(error)")
         }
     }
 }
